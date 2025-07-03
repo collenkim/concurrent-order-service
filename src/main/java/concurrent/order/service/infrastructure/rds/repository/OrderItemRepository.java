81820fd2ce7dc3dda179a16651a275e89f465262
collenkim <sasin8712@naver.com>
2025-07-02 23:19:01 -0700

    @Repository 어노테이션 제거

--- src/main/java/concurrent/order/service/infrastructure/rds/repository/OrderItemRepository.java
+++ src/main/java/concurrent/order/service/infrastructure/rds/repository/OrderItemRepository.java
@@ -2,9 +2,7 @@ package concurrent.order.service.infrastructure.rds.repository;
 
 import concurrent.order.service.infrastructure.rds.entity.OrderItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long> {
 
 }
